e1d3798c1b1363653048591f19d6bbd657b4ca27
Deepthi S <dees3@publicisgroupe.net>
2024-03-30 15:37:27 +0530

    lc 334

---
+++ inccreasingTriplets.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var increasingTriplet = function(nums) {
+    let a = Infinity, b = Infinity, c = Infinity;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] <= a) a = nums[i];
+        else if (nums[i] <= b) b = nums[i];
+        else if (nums[i] <= c) c = nums[i];
+
+        if (c < Infinity && c > b && b > a) return true;
+    }
+    return false;
+};
