5d9e3c69690e968b818f127466d8bea40bffb214
Deepthi S <dees3@publicisgroupe.net>
2024-03-30 15:24:47 +0530

    lc 20

---
+++ validParenthesis.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack =[];
+    const map = {'(': ')', '{':'}', '[':']'};
+    for(let ch of s){
+        if(map[ch]){
+            stack.push(map[ch]);
+        } else if(stack.length > 0 && stack[stack.length -1] === ch){
+            stack.pop();
+        } else return false
+    }
+    return (stack.length === 0);
+};
